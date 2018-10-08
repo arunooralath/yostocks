364e8c9f528d145d8e5910b5f6e65b9e664d0bc6
Shinil MS <shinil.ms@sulfursouth.com>
2018-10-09 01:04:29 +0530

    Yo stocks initial commit, Created api for signup and login

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const userRoutes = require('./api/routes/user');
+
+mongoose.connect(
+    "mongodb://root:" +
+    process.env.YS_MDB_PW +
+    "@ds125423.mlab.com:25423/yostocks",
+    {
+        useMongoClient: true
+    }
+);
+
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+// Routes which should handle requests
+// app.use("/products", productRoutes);
+// app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It works!'
+    });
+});
+
+module.exports = app;
