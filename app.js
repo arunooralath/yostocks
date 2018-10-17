c688a1485567283c46698d914a2f29580e902a38
Shinil MS <shinil.ms@sulfursouth.com>
2018-10-17 14:57:09 +0530

    Changes dotenv

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./api/routes/user');
 const productRoutes = require("./api/routes/products");
-var dotenv = require('dotenv');
+const dotenv = require('dotenv');
 dotenv.load();
 
 mongoose.connect(
